b315ba3f680ed164adea4fadeb49ac888f3873d9
Zahra Hosseini <zrhos1998@gmail.com>
2026-09-04 18:54:22 +0400

    1405-06-13

---
+++ main.js
@@ -0,0 +1,3 @@
+document.querySelector(".city").addEventListener("click",function(){document.querySelector(".modal").classList.add("active")})
+
+document.querySelector(".close-btn").addEventListener("click",function(){document.querySelector(".modal").classList.remove("active")})
